98f7154bd984ceee857eeb3e558b18431f404756
Anatoly Karpov <148396599+anatolykarpov1221@users.noreply.github.com>
2024-04-25 22:18:52 +0200

    add edit but with error

--- src/test/java/com/phonebook/testsRA/EditContactsTests.java
+++ src/test/java/com/phonebook/testsRA/EditContactsTests.java
@@ -1,14 +1,11 @@
 package com.phonebook.testsRA;
-
 import com.phonebook.dto.ContactDto;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
-public class DeleteContactByIdTests extends TestBase{
+public class EditContactsTests extends TestBase {
     String id;
 
     @BeforeMethod
@@ -29,35 +26,49 @@ public class DeleteContactByIdTests extends TestBase{
                 .then()
                 .assertThat().statusCode(200)
                 .extract().path("message");
-           System.out.println(message);
+        System.out.println(message);
         //Contact was added! ID: 01b40aac-2b5f-4f28-8c95-d10bb0db4171
         String[] split = message.split(": ");
-        id= split[1];
-
-        }
-
-
-    @Test
-    public void deleteContactByIdSuccessTest(){
-
-        given()
-                .header(AUTH, TOKEN)
-                .delete("contacts/" + id)
-                .then()
-                .assertThat().statusCode(200)
-                //.extract().path(("message"));
-        //System.out.println(message);
-                .assertThat().body("message",equalTo("Contact was deleted!"));
+             id= split[1];
 
     }
-    @Test
-    public void deleteContactByInvalidIdTest() {
+
+
+   @Test
+    public void editContactSuccessTest() {
+        // Assuming the contact with id 'id_to_edit' exists
+        //String id_to_edit = id;
+
         given()
                 .header(AUTH, TOKEN)
-                .delete("contacts/" + "false_id")
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Nikolay\", \"phone\": \"1558997339\"}")
+
+                // JSON как правильно написать для  update?
+                .when()
+                .put("contacts/" + id)
                 .then()
-                .assertThat().statusCode(400)
-            .assertThat().body("message", equalTo("Contact with id: false_id not found in your contacts!"))
-        ;}
+                .assertThat().statusCode(200);
+
+        System.out.println("Contact with ID " + id + " has been successfully updated.");
+    }
+
+    @Test
+    public void editContactNotFoundTest() {
+
+        String id_not_found = "NON_EXISTENT_ID"+id;
+
+        given()
+                .header(AUTH, TOKEN)
+                .contentType(ContentType.JSON)
+                .body("{ name: Nikolay, phone:1558997339 }")
+                //   ???
+                .when()
+                .put("contacts/" + id_not_found)
+                .then()
+                .assertThat().statusCode(404);
+
+        System.out.println("Attempted to edit a non-existent contact with ID " + id_not_found);
+    }
 
 }
